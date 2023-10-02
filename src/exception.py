4acf640d496fd4f8643f0aa78841c797409b12c6
Rajat Goswami <rajatgoswami7333@gmai.com>
2023-10-02 12:40:23 +0530

    Data Transformation is done

--- src/exception.py
+++ src/exception.py
@@ -1,24 +1,24 @@
-import sys
-import logging
-from src.logger import logging
+# import sys
+# import logging
+# from src.logger import logging
 
-#called whenever any exception arrises
-def error_message_detail(error, error_detail :sys ):
-    #gives us the execution info and will give 3 imp info
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
-    )
-    return error_message
+# #called whenever any exception arrises
+# def error_message_detail(error, error_detail :sys ):
+#     #gives us the execution info and will give 3 imp info
+#     _,_,exc_tb = error_detail.exc_info()
+#     file_name = exc_tb.tb_frame.f_code.co_filename
+#     error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+#         file_name,exc_tb.tb_lineno,str(error)
+#     )
+#     return error_message
 
-class CustomExeption(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+# class CustomExeption(Exception):
+#     def __init__(self, error_message, error_detail:sys):
+#         super().__init__(error_message)
+#         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
-        def __str__(self):
-            return self.error_message
+#         def __str__(self):
+#             return self.error_message
 
 # if __name__ == '__main__':
 #     try:
@@ -26,3 +26,27 @@ class CustomExeption(Exception):
 #     except Exception as e:
 #         logging.info("Divide by 0")
 #         raise CustomExeption(e,sys)
+import sys
+from src.logger import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
+
+
+        
